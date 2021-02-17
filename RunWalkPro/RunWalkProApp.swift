610dd648e4799d5f4c37875ce7f6d43230395525
Marc Mendoza <777marc@gmail.com>
2021-02-17 16:11:51 -0500

    Initial Commit

---
+++ RunWalkPro/RunWalkProApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunWalkProApp.swift
+//  RunWalkPro
+//
+//  Created by Marc Mendoza on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct RunWalkProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
